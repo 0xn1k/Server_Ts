135ea55fbe720538fc9d08cd88b826fe1f0454ba
nishantdotcom <nishantsingh1308@gmail.com>
2025-10-05 14:39:20 +0530

    introducing req.body in http-server

--- src/advanceServer.ts
+++ src/advanceServer.ts
@@ -17,28 +17,70 @@ interface loggerInterface {
   (info: logger) : void;
 }
 
+interface MyServerRequest extends http.IncomingMessage {
+  body?: any;
+}
+
+function jsonPareser(req:MyServerRequest, res:http.ServerResponse, next: ()=>void){
+  let body = '';
+  console.log("Inside JSON parser middleware");
+  if(req.headers['content-type'] === 'application/json'){
+    req.on('data', chunk => {
+      body += chunk.toString(); 
+    });
+    req.on('end', ()=>{
+      try{
+        req.body = JSON.parse(body);
+      } catch (e){
+        req.body = {};
+        return;
+      }
+      next();
+    })
+    next();
+    
+  }
+  else{
+    next();
+  }
+}
 
 function myOwnServer(){
 
     const routes : Route[] = [];
+    const middlewares : ((req:MyServerRequest, res:http.ServerResponse, next: ()=>void)=>void)[] = [];
 
-    function get(path: string ,handler :(req:http.IncomingMessage, res:http.ServerResponse)=>void){
+    function get(path: string ,handler :(req:MyServerRequest, res:http.ServerResponse)=>void){
         routes.push({path, method: "GET", handler});
     }
     
-    function post(path:string , handler : (req:http.IncomingMessage , res: http.ServerResponse)=>void){
+    function post(path:string , handler : (req:MyServerRequest , res: http.ServerResponse)=>void){
         routes.push({path, method: "POST", handler});
     }
 
-    function put(path:string , handler : (req:http.IncomingMessage , res: http.ServerResponse)=>void){
+    function put(path:string , handler : (req:MyServerRequest , res: http.ServerResponse)=>void){
         routes.push({path, method: "PUT", handler});
     }
 
-    function del(path:string , handler : (req:http.IncomingMessage , res: http.ServerResponse)=>void){
+    function del(path:string , handler : (req:MyServerRequest , res: http.ServerResponse)=>void){
         routes.push({path, method: "DELETE", handler});
     } 
 
-    function handler(req:http.IncomingMessage, res :http.ServerResponse) {
+    function handler(req:MyServerRequest, res :http.ServerResponse) {
+      let idx = 0;
+      function next(){
+        const middleware = middlewares[idx];
+        idx++;
+        if(middleware){
+          middleware(req, res, next);
+        } else {
+          processRequest(req, res);
+        }
+      }
+      next();
+    
+     function processRequest(req:MyServerRequest, res:http.ServerResponse){
+        console.log("Request received:", req.method, req.url);
         const route = routes.find((r: Route) => r.method === req.method && r.path === req.url);   
         if (route) {
         route.handler(req, res);
@@ -48,6 +90,11 @@ function myOwnServer(){
         res.statusCode = 404;
         res.end('Not Found');
         }
+      }
+    }
+
+    function use(middleware: (req:MyServerRequest, res:http.ServerResponse, next: ()=>void)=>void)  {
+      middlewares.push(middleware);
     }
 
     const logger : loggerInterface = (info: logger) => {
@@ -59,17 +106,19 @@ function myOwnServer(){
     const server = http.createServer(handler);
     server.listen(port, callback);
   }
-  return { get, listen ,post , put , del };
+  return { get, listen ,post , put , del  , use };
 }
 
 const app = myOwnServer();
+
+app.use(jsonPareser);
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  res.end('Hello from custom mini-framework 🚀');
+  res.end('Hello from / directory 🚀');
 });
 app.get('/test', (req, res) => {
   res.statusCode = 200;
-  res.end('Hello from custom mini-framework 🚀');
+  res.end('Hello from /test route 🚀');
 });
 
 app.put('/test-put', (req, res) => {
@@ -78,6 +127,7 @@ app.put('/test-put', (req, res) => {
 });
 
 app.post('/test-post', (req, res) => {
+ console.log('Request Body:', req.body);
   res.statusCode = 200;
   res.end('POST request received');
 });
