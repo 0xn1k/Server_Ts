f9ed2d28a4b30254df8f1cc953888fa6e69394f6
nishantdotcom <nishantsingh1308@gmail.com>
2025-10-05 12:36:57 +0530

    adding logger

--- src/advanceServer.ts
+++ src/advanceServer.ts
@@ -4,8 +4,18 @@ import http from "http";
 type Route = {
   method: string;
   path: string;
-  handler: (req: any, res: any) => void;
+  handler: (req: any, res: any) => void; 
 };
+
+type logger ={
+  status : number;
+  method : string;
+  path : string;
+}
+
+interface loggerInterface {
+  (info: logger) : void;
+}
 
 
 function myOwnServer(){
@@ -13,49 +23,68 @@ function myOwnServer(){
     const routes : Route[] = [];
 
     function get(path: string ,handler :(req:http.IncomingMessage, res:http.ServerResponse)=>void){
-        console.log("Registering GET route:", path);
-        console.log("Current routes:", routes);
-        console.log("methods", routes.map(r=>r.method));
         routes.push({path, method: "GET", handler});
-        console.log("Updated routes:", routes);
+    }
+    
+    function post(path:string , handler : (req:http.IncomingMessage , res: http.ServerResponse)=>void){
+        routes.push({path, method: "POST", handler});
     }
 
+    function put(path:string , handler : (req:http.IncomingMessage , res: http.ServerResponse)=>void){
+        routes.push({path, method: "PUT", handler});
+    }
+
+    function del(path:string , handler : (req:http.IncomingMessage , res: http.ServerResponse)=>void){
+        routes.push({path, method: "DELETE", handler});
+    } 
+
     function handler(req:http.IncomingMessage, res :http.ServerResponse) {
-        const route = routes.find(
-       (r: Route) => r.method === req.method && r.path === req.url);   
+        const route = routes.find((r: Route) => r.method === req.method && r.path === req.url);   
         if (route) {
         route.handler(req, res);
+        logger({status: res.statusCode, method: req.method, path: req.url} as logger)
         } else {
+        logger({status: 404, method: req.method, path: req.url} as logger)
         res.statusCode = 404;
         res.end('Not Found');
         }
-  }
+    }
 
-  // Start server
+    const logger : loggerInterface = (info: logger) => {
+      const log = `[${new Date().toISOString()}] ${info.method} ${info.path} - ${info.status}`;
+      console.log(log);
+    }
+
   function listen(port : number, callback : ()=>void) {
-    console.log("Starting server on port", port);
-    console.log("Registered routes:", routes);
     const server = http.createServer(handler);
     server.listen(port, callback);
   }
-
-  return { get, listen };
-
-
+  return { get, listen ,post , put , del };
 }
 
 const app = myOwnServer();
-
 app.get('/', (req, res) => {
-  console.log("hiii")
+  res.statusCode = 200;
+  res.end('Hello from custom mini-framework 🚀');
+});
+app.get('/test', (req, res) => {
   res.statusCode = 200;
   res.end('Hello from custom mini-framework 🚀');
 });
 
-app.get('/test', (req, res) => {
-  console.log("hiii")
+app.put('/test-put', (req, res) => {
   res.statusCode = 200;
-  res.end('Hello from custom mini-framework 🚀');
+  res.end('PUT request received');
+});
+
+app.post('/test-post', (req, res) => {
+  res.statusCode = 200;
+  res.end('POST request received');
+});
+
+app.del('/test-delete', (req, res) => {
+  res.statusCode = 200;
+  res.end('DELETE request received');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
